9a429270b3e149d5f15647a4587f1d87b842f9bb
xsalles <xs.salles@gmail.com>
2025-09-25 23:51:03 -0300

    feat: created the model of user entity

---
+++ Model/User.cs
@@ -0,0 +1,9 @@
+namespace AuthDotNetApi.Model
+
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- obj/Debug/net9.0/auth-dotnet-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/auth-dotnet-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("auth-dotnet-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989c1f3aa2359399f5f873b0f804524750723c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("auth-dotnet-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("auth-dotnet-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
